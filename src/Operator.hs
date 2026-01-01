ddbf01bea93bb55f5ce83b9e87139d52960b1650
Rajil Bajracharya <imrb1213@gmail.com>
2026-01-01 18:50:20 +0545

    chore(osm): format code

--- src/Operator.hs
+++ src/Operator.hs
@@ -1,14 +1,14 @@
-module Operator 
-    ( OperatorState
-    , updateOperatorTable
-    , notifyNewBlock
-    , getCurrentOperatorTable
-    , getMasterOperatorTable
-    , getPendingUpdates
-    ) where
+module Operator (
+  OperatorState,
+  updateOperatorTable,
+  notifyNewBlock,
+  getCurrentOperatorTable,
+  getMasterOperatorTable,
+  getPendingUpdates,
+) where
 
 -- Prelude
-import qualified Data.Set as Set
+import Data.Set qualified as Set
 import Data.Word
 
 type U32 = Data.Word.Word32
@@ -22,7 +22,8 @@ type AdaptorKey = String -- placeholder
 type OperatorRecord = (OperatorIdx, P2PKey, SchnorrKey, AdaptorKey) -- ordered by `OperatorIdx`
 -- a `Data.Set` in Haskell orders tuples by default based on the first member
 -- in Rust, use `BTreeSet` to get the same property
-type OperatorTable = Set.Set OperatorRecord 
+
+type OperatorTable = Set.Set OperatorRecord
 type MasterTable = OperatorTable
 type CurTable = OperatorTable
 type NewTable = OperatorTable
@@ -33,42 +34,41 @@ type PendingUpdates = Set.Set PendingUpdate
 -- State
 data OperatorState
   = Ready MasterTable CurTable
-    | UpdateReceived MasterTable CurTable PendingUpdates
+  | UpdateReceived MasterTable CurTable PendingUpdates
 
 -- STF
 updateOperatorTable :: OperatorState -> ActivationHeight -> NewTable -> OperatorState
-updateOperatorTable curState activationHeight newTable
-  = let newPendingUpdate = Set.singleton (activationHeight, newTable)
-        (masterTable, curTable, pendingUpdates) = case curState of
-          Ready m c            -> (m, c, Set.empty)
-          UpdateReceived m c p -> (m, c, p)
-    in UpdateReceived 
+updateOperatorTable curState activationHeight newTable =
+  let newPendingUpdate = Set.singleton (activationHeight, newTable)
+      (masterTable, curTable, pendingUpdates) = case curState of
+        Ready m c -> (m, c, Set.empty)
+        UpdateReceived m c p -> (m, c, p)
+   in UpdateReceived
         (newTable `Set.union` masterTable)
-        curTable 
+        curTable
         (pendingUpdates `Set.union` newPendingUpdate)
-   
+
 -- STF
 notifyNewBlock :: OperatorState -> Height -> OperatorState
-
 -- not affected
-notifyNewBlock (Ready masterTable curTable) _height
-  = Ready masterTable curTable
-
-notifyNewBlock (UpdateReceived masterTable curTable pendingUpdates) height
-  = -- find the first instance where `height` >= `activationHeight`
-    case Set.lookupGE (height, Set.empty) pendingUpdates of
-      Just (activationHeight, newTable) 
-        -- if this was the only pending update, then transition to ready
-        -> if length pendingUpdates == 1 then Ready masterTable newTable
-          -- if there are other pending updates, 
-          -- then just update the `curTable` and `pendingUpdates`
-          else UpdateReceived 
-                masterTable 
-                newTable 
-                (Set.delete (activationHeight, newTable) pendingUpdates) 
-      Nothing 
-        -> UpdateReceived masterTable curTable pendingUpdates -- no viable update found
-
+notifyNewBlock (Ready masterTable curTable) _height =
+  Ready masterTable curTable
+notifyNewBlock (UpdateReceived masterTable curTable pendingUpdates) height =
+  -- find the first instance where `height` >= `activationHeight`
+  case Set.lookupGE (height, Set.empty) pendingUpdates of
+    Just (activationHeight, newTable) ->
+      -- if this was the only pending update, then transition to ready
+      if length pendingUpdates == 1
+        then Ready masterTable newTable
+        -- if there are other pending updates,
+        -- then just update the `curTable` and `pendingUpdates`
+        else
+          UpdateReceived
+            masterTable
+            newTable
+            (Set.delete (activationHeight, newTable) pendingUpdates)
+    Nothing ->
+      UpdateReceived masterTable curTable pendingUpdates -- no viable update found
 
 -- Introspection functions (not part of the STF)
 getCurrentOperatorTable :: OperatorState -> OperatorTable
